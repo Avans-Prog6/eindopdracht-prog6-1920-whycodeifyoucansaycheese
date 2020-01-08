ecbb983471d7de99018a3ff63173d57c660276e8
Niels <nga.smits@student.avans.nl>
2020-01-08 11:16:27 +0100

    pushhhhe

--- BeestjeOpJeFeestje/BeestjeOpJeFeestje.Tests/Controllers/BookingControllerTest.cs
+++ BeestjeOpJeFeestje/BeestjeOpJeFeestje.Tests/Controllers/BookingControllerTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using BeestjeOpJeFeestje.Controllers;
+using BeestjeOpJeFeestje.Domain;
 using BeestjeOpJeFeestje.Domain.Interface_Repositories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -10,6 +12,7 @@ namespace BeestjeOpJeFeestje.Tests.Controllers
     public class BookingControllerTest
     {
         private Mock<IBoekingRepository> _boekingsRepository;
+        private Mock<IBeastRepository> _beastRepository;
         //private Mock<ApplicationUserManager> _userManager;
         //private Mock<ApplicationSignInManager> _signInManager;
         private BookingController _bookingscontroller;
@@ -19,6 +22,48 @@ namespace BeestjeOpJeFeestje.Tests.Controllers
         public void Init()
         {
             _boekingsRepository = new Mock<IBoekingRepository>();
+            _beastRepository = new Mock<IBeastRepository>();
+
+        }
+
+        public void BookPinguinInWeekend_False_Test()
+        {
+            //1. Arrange
+            Mock<Beast> pin = new Mock<Beast>();
+            pin.Setup(p => p.Name == "Pinguin");
+            var list = _beastRepository.Object.BeastsAvailable(DateTime.Parse("16/02/2008")).ToList();
+            //2. Act
+
+            bool IsIn = list.Contains(pin.Object);
+
+        }
+
+        private Booking GetBadSampleBooking()
+        {
+            Booking booking = new Booking();
+            List<Beast> beasts = new List<Beast>
+            {
+                new Beast
+                {
+                    Name = "Koe",
+                    Price = 100,
+                    Type = "Boerderij"
+                },
+                new Beast
+                {
+                    Name = "Paard",
+                    Price = 100,
+                    Type = "Boerderij"
+                },
+                new Beast
+                {
+                    Name = "Pinguin",
+                    Price = 100,
+                    Type = "Woestijn"
+                }
+            };
+            booking.Beast = beasts;
+            return booking;
         }
     }
 }
